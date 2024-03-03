14eddee76e4ec25dbae8f4a035be21cd706b577a
urraccon <133591634+urraccon@users.noreply.github.com>
2024-03-03 10:23:48 +0200

    added storage in local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,24 +6,38 @@ import { Filter } from './filter/Filter';
 import { ContactList } from './contact_list/ContactList';
 import { Section } from './common/section/Section';
 
+const CONTACTS_KEY = 'contacts';
+
 export class App extends Component {
   state = {
     // contacts: [],
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     // contactsCopy: [],
-    contactsCopy: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contactsCopy: [],
     filter: '',
   };
+
+  async componentDidMount() {
+    const data = localStorage.getItem(CONTACTS_KEY);
+    // console.log(JSON.parse(data));
+    try {
+      if (data) {
+        this.setState({
+          contacts: JSON.parse(data),
+          contactsCopy: JSON.parse(data),
+        });
+      }
+    } catch (err) {
+      console.log(`error: ${err}`);
+    }
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    // console.log('updating component');
+    if (prevState?.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem(CONTACTS_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = form => {
     // debugger;
@@ -113,3 +127,10 @@ export class App extends Component {
     );
   }
 }
+
+// data
+
+// { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
